31d4b9629fdb4dd23bd5f838f2ed9ebd957dc3c5
Sengankou <ssoqazan@gmail.com>
2024-02-05 21:02:31 +0900

    Laravel Breezeのインストールとそれに必要な設定

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,8 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        // ホットリロード(自動更新)がならない場合追加
+        hmr: {
+            host: 'localhost',
+        },
+        // CSSの自動反映がならない場合追加(Dockerだと多少時差がでるかも)
+        watch: {
+          usePolling: true,
+        },
+    },
 });
