62fcf261869bb17d47557ced37833cbfefbe48a2
Sengankou <ssoqazan@gmail.com>
2024-02-08 14:02:34 +0900

    chore: fontの調整

--- src/resources/views/livewire/subcategories/show.blade.php
+++ src/resources/views/livewire/subcategories/show.blade.php
@@ -5,7 +5,7 @@
             @foreach ( $subcategories as $subcategory)
                 <li class="my-2 px-3 py-1 bg-indigo-800/40 overflow-hidden shadow-none sm:rounded-lg ring-2 ring-inset ring-blue-300/60">
                     <div>
-                        <h3 class="text-xl font-semibold">
+                        <h3 class="text-xl font-medium">
                             {{ $subcategory->name }}
                         </h3>
                     </div>
